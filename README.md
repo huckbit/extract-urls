c5b1e44be4162bca801bd6dcf9e2e05e90b48463
huckbit <huckbit@gmail.com>
2020-08-12 23:30:08 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # extract urls
+[![Build Status](https://travis-ci.org/huckbit/extractUrls.svg?branch=master)](https://travis-ci.org/huckbit/extractUrls)
 
 extracts all the urls (http and https) from a given string
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const extractUrls = require("./lib/extracUrls");
 
 try {
-  console.log(extractUrls(``));
+  console.log(extractUrls(`Links https://www.google.com/ and https://www.google.co.uk/`));
 } catch (e) {
   console.log(e);
 }
