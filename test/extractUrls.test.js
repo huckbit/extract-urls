66e1ef67fa7ef495910b44e9022ca6c7c811921f
huckbit <huckbit@gmail.com>
2020-08-12 23:48:28 +0100

    refactoring & info updated

--- test/extractUrls.test.js
+++ test/extractUrls.test.js
@@ -1,7 +1,10 @@
 const extractUrls = require("../lib/extracUrls");
 
 test("get urls from the given string", () => {
+
   const str = `this string contains to urls. This is the first one https://www.huckbit.com the second one https://cssloaders.com`;
   const expected = ["https://www.huckbit.com", "https://cssloaders.com"];
+
   expect(extractUrls(str)).toEqual(expect.arrayContaining(expected));
+
 });
